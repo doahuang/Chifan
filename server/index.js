cfac0411f4988db6fa5f3499eb0b056bda77d06d
doahuang <doahuang@gmail.com>
2018-08-22 18:47:18 -0700

    Added cookie session and logout

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 const app = express();
+
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./models/User');
 mongoose.connect(keys.mongoURI);
