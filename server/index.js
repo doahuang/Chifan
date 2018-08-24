2a74cfb04cd93987a88cdb310f9ba1f35533d1e3
doahuang <doahuang@gmail.com>
2018-08-24 13:47:43 -0700

    Added production deployment

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const keys = require('./config/keys');
-
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const keys = require('./config/keys');
+
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
@@ -11,15 +15,19 @@ app.use(cookieSession({
   maxAge: 30 * 24 * 60 * 60 * 1000,
   keys: [keys.cookieKey]
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./models/User');
-mongoose.connect(keys.mongoURI);
-
-require('./services/passport');
 require('./routes/authRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server runs on locahost:${PORT}`));
