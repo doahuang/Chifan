e48cee9f55239a33faad77317d044da0db59a569
doahuang <doahuang@gmail.com>
2018-08-24 15:47:37 -0700

    Set up survey routes

---
+++ server/routes/surveyRoutes.js
@@ -0,0 +1,24 @@
+const requireLogin = require('../middlewares/requireLogin');
+
+const mongoose = require('mongoose');
+const Survey = mongoose.model('surveys');
+
+module.exports = app => {
+  app.get('/api/surveys', (req, res) => res.send('Thank you'));
+
+  app.post('/api/surveys', requireLogin, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+
+    await survey.save();
+    res.send(survey);
+  });
+};
