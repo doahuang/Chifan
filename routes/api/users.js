7d0bc3b358c18ecd08a38562705e4d80916c95fe
doahuang <doahuang@gmail.com>
2018-09-13 18:08:04 -0700

    updated user routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({ msg: 'users route' });
+const User = require('../../models/user');
+
+router.get('/', async (req, res) => {
+  const users = await User.find({});
+  return res.json(users);
+});
+
+router.post('/signup', async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(400).json({ err: 'User existed' });
+  }
+  
+  const { name, email, password } = req.body;
+  user = new User({
+    name, 
+    email, 
+    password
+  });
+
+  user.save();
 });
 
 module.exports = router;
