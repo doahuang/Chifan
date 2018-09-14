79c698a5f1469365aec3c3c5481c9d306d490ab3
doahuang <doahuang@gmail.com>
2018-09-13 17:23:42 -0700

    added routes

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const db = require('./config/keys').mongoURI;
 mongoose.connect(db).then(() => console.log('Connected to db'));
 
 const app = express();
 
-app.get('/',(req, res) => {
-  res.send('Hello World!');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const users = require('./routes/api/users');
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
